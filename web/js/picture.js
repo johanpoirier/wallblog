e5c2cf53940ba261fb3790f6e1b35e8effeec855
Johan Poirier <johan.poirier@gmail.com>
2012-02-13 21:46:17 +0100

    fix tests

--- web/js/picture.js
+++
@@ -1,8 +0,0 @@
-define('picture', ["jquery"], function($) {
-    return {
-        'loading' : false,
-        'getItems' : function(callback, start, nb) {
-            $.getJSON("/api/items?start=" + start + "&nb=" + nb, callback);
-        }
-    }
-});
