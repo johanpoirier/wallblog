19287e64764977fa0d2247b2cda7db750d377f61
Johan Poirier <johan.poirier@gmail.com>
2012-02-27 14:41:47 +0100

    Anonymizing db config.

--- src/config.php
+++ src/config.php
@@ -1,6 +1,6 @@
 <?php
-$config["host"] = "mysql5-13";
-$config["dbname"] = "nirgal";
-$config["user"] = "nirgal";
-$config["password"] = "9d9byeld";
+$config["host"] = "localhost";
+$config["dbname"] = "wallblog";
+$config["user"] = "root";
+$config["password"] = "";
 ?>
