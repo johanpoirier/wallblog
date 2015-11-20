7169c0c87913099496c70c4213bc4b1f4f5fc999
Johan Poirier <johan.poirier@gmail.com>
2015-11-20 16:09:04 +0100

    Chg: docker image is ready

--- docker/run.sh
+++ docker/run.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 IMAGE_NAME='wallblog'
-CID_FILE="/tmp/$IMAGE_NAME.cid"
+CID_FILE="/tmp/${IMAGE_NAME}.cid"
 SCRIPT_DIR=$(dirname "$0")
+WORKSPACE=`pwd`/..
+DOCKER_WORKSPACE='/var/www/wallblog'
+GULP_LOG="${DOCKER_WORKSPACE}/logs/gulp.log"
+
 
 rm -f "$CID_FILE"
-docker run -d --cidfile="$CID_FILE" -v "$WORKSPACE/ecommerce/":/var/www/ecommerce/current:rw -w /var/www/ecommerce/current --name $IMAGE_NAME $IMAGE_NAME
+
+echo "Running docker container"
+docker run -d --cidfile="$CID_FILE" -v "${WORKSPACE}":${DOCKER_WORKSPACE}:rw -w ${DOCKER_WORKSPACE} --name $IMAGE_NAME $IMAGE_NAME
+echo ""
 
 while [ ! -f "$CID_FILE" ]
 do
   sleep 1
 done
 
+echo "Building wallblog app:"
+docker exec $IMAGE_NAME ${DOCKER_WORKSPACE}/docker/wallblog.sh
+echo "Done."
+echo ""
+
 CONTAINER_ID=$(cat "$CID_FILE")
 CONTAINER_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $CONTAINER_ID)
 
-echo "Go to  ${CONTAINER_IP} to test wallblog"
+echo "Go to http://${CONTAINER_IP} to test wallblog"
---
+++ docker/wallblog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /var/www/wallblog
+npm install
+echo " - npm OK"
+bower --allow-root install
+echo " - bower OK"
+rm -f ./logs/gulp.log
+touch ./logs/gulp.log
+gulp > ./logs/gulp.log 2>&1 &
+echo " - gulp OK"
