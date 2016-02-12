c3354bb75b739021262743753f80581b88ba499b
Johan Poirier <johan.poirier@gmail.com>
2016-02-12 13:21:17 +0100

    Add: sample pictures for docker image

--- docker/wallblog.sh
+++ docker/wallblog.sh
@@ -2,3 +2,5 @@
 
 cd /var/www/wallblog
 npm install
+
+mv /var/www/pictures/* /var/www/wallblog/dist/pictures/
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
 
-HOST='ftp.60gp.ovh.net'
-USER='nirgal'
-PASSWD='9d9byeld'
-FILES='file.txt'
-DIR='wallblog'
-
-
-ftp -n $HOST <<END_SCRIPT
-quote USER $USER
-quote PASS $PASSWD
-cd $DIR
-put dist/index.html /wallblog/dist/index.html
-quit
-END_SCRIPT
-exit 0
