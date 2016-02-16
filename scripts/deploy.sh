d6c591f97b186689ffbcb14899eaac8d5d7acb90
Johan Poirier <johan.poirier@gmail.com>
2016-02-16 13:15:14 +0100

    Add: deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-tokenFile='/tmp/WALLBLOG'
+projectDir="$(dirname $0)/../"
 
-if grep -Fxq "UPDATE" $tokenFile
+target=$1
+targetPath=~/$target
+
+if [ -e $targetPath ]
 then
-    echo "[Updating wallblog] `date '+%F-%T'`\n"
-    cd ~/wallblog
-    git pull origin master
-    npm install
-    npm run package
-    echo "DONE" > $tokenFile
+    version=`date '+%Y%m%d%H%M%S'`
+    mkdir -p "$targetPath/versions/$version/"
+    rm -f "$targetPath/current"
+    ln -s "$targetPath/versions/$version/" "$targetPath/current"
+
+    pushd "$projectDir"
+    cp -r dist/ "$targetPath/current/dist/"
+    rm -rf "$targetPath/current/dist/pictures/"
+    ln -s "$targetPath/pictures/" "$targetPath/current/dist/pictures"
+    mkdir "$targetPath/current/logs"
+    chmod 775 "$targetPath/current/logs"
+    cp -r vendor/ "$targetPath/current/vendor/"
+    cp -r views/ "$targetPath/current/views/"
+    cp -r src/ "$targetPath/current/src/"
+    cp "$targetPath/config.php" "$targetPath/current/src/config.php"
+    chgrp -R www-data "$targetPath/current/"
+    popd
+
+    echo "Version $version created"
+else
+    echo "$target does not exist"
 fi
---
+++ scripts/update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+tokenFile='/tmp/WALLBLOG'
+
+if grep -Fxq "UPDATE" $tokenFile
+then
+    echo "\n\n[Updating wallblog] `date '+%F-%T'`\n"
+    cd ~/wallblog
+    git pull origin master
+    npm install
+    npm run package
+    echo "DONE" > $tokenFile
+fi
