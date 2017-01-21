1c1ed16af32e63963e6bac6d87eef57ecedd4474
Johan Poirier <johan.poirier@gmail.com>
2017-01-21 14:58:42 +0100

    notify clients

--- src/services/SubscriptionService.php
+++ src/services/SubscriptionService.php
@@ -4,17 +4,6 @@ namespace services;
 
 use Minishlink\WebPush\WebPush;
 
-$config = json_decode(file_get_contents(__DIR__ . '/../../config.json'), true);
-
-// array of notifications
-$serverConfig = [
-  'VAPID' => [
-    'subject' => $config['private']['vapidEmail'],
-    'publicKey' => $config['public']['vapidPublicKey'],
-    'privateKey' => $config['private']['vapidPrivateKey']
-  ]
-];
-
 /**
  * Description of SubscriptionService
  *
@@ -25,12 +14,19 @@ class SubscriptionService {
     private static $db;
     private static $table_name;
     private static $logger;
+    private static $push;
 
     public function __construct($db, $app_logger, $config) {
         self::$db = $db;
         self::$logger = $app_logger;
         self::$table_name =  $config["prefix"] . "__subscription";
-        self::$push = new WebPush($serverConfig);
+
+        $vapidConfig = json_decode(file_get_contents(__DIR__ . '/../../config.life.json'), true);
+        self::$push = new WebPush([ 'VAPID' => [
+          'subject' => $vapidConfig['private']['vapidEmail'],
+          'publicKey' => $vapidConfig['public']['vapidPublicKey'],
+          'privateKey' => $vapidConfig['private']['vapidPrivateKey']
+        ]]);
     }
 
     public function create($subscriptionData) {
@@ -45,11 +41,11 @@ class SubscriptionService {
     }
 
   public function getAll() {
-    $sql = "SELECT * FROM $this->table_name subscription";
-    return $this->db->fetchAll($sql);
+    $sql = "SELECT * FROM " . self::$table_name;
+    return self::$db->fetchAll($sql);
   }
 
-  public notify($item) {
+  public function notify($item) {
     $subscriptions = self::getAll();
     for ($i = 0; $i < sizeof($subscriptions); $i++) {
       $subscription = $subscriptions[$i];
