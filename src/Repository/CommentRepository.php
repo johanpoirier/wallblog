7c6c5b46624b53621b3db9b10a946a908d39ead7
Johan Poirier <johan.poirier@gmail.com>
2018-10-29 12:13:47 +0100

    Add RSS routes

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -8,11 +8,13 @@ class CommentRepository
 {
   private $db;
   private $tableName;
+  private $itemTableName;
 
   public function __construct(Connection $db, string $prefix)
   {
     $this->db = $db;
     $this->tableName = "${prefix}__comment";
+    $this->itemTableName = "${prefix}__item";
   }
 
   /**
@@ -23,6 +25,23 @@ class CommentRepository
   {
     $sql = "SELECT * FROM  $this->tableName WHERE id = ?";
     return $this->db->fetchAssoc($sql, [$id]);
+  }
+
+  /**
+   * @param $count
+   * @param $index
+   * @return mixed
+   */
+  public function findPaginated($count, $index)
+  {
+    $request = <<<SQL
+SELECT c.id, c.text, c.author, c.date, c.idItem, i.description, i.file
+FROM $this->tableName c
+LEFT JOIN $this->itemTableName i ON (c.idItem = i.id)
+ORDER BY c.date DESC
+LIMIT $index, $count;
+SQL;
+    return $this->db->fetchAll($request);
   }
 
   /**
